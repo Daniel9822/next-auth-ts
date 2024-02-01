0ce1af31938f7af9b43d4cdc1bf506025e699651
Daniel9822 <dhdany64@gmail.com>
2024-02-01 19:54:05 -0400

    feat: react particles

---
+++ src/utils/particlesBouces.tsx
@@ -0,0 +1,104 @@
+'use client'
+import { useContext, useEffect, useMemo, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { type Container, type ISourceOptions } from '@tsparticles/engine'
+import { loadSlim } from '@tsparticles/slim'
+import { ThemeContext } from '@/context/ThemeContext'
+
+export default function ParticlesComponentBounce() {
+  const { mode } = useContext(ThemeContext)
+  const [init, setInit] = useState(false)
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine)
+    }).then(() => {
+      setInit(true)
+    })
+  }, [])
+
+  let particlesLoaded = async (container?: Container): Promise<void> => {}
+
+  let options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: 'transparent'
+        }
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'push'
+          },
+          onHover: {
+            enable: true,
+            mode: 'repulse'
+          }
+        },
+        modes: {
+          push: {
+            quantity: 4
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: mode === 'dark' ? '#ffffff' : '#000'
+        },
+        bounce: {
+          color: mode === 'dark' ? '#ffffff' : '#000',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outModes: {
+            default: 'out'
+          },
+          random: false,
+          speed: 6,
+          straight: false
+        },
+        number: {
+          density: {
+            enable: true
+          },
+          value: 80
+        },
+        opacity: {
+          value: 0.5
+        },
+        shape: {
+          type: 'circle'
+        },
+        size: {
+          value: { min: 1, max: 5 }
+        }
+      },
+      detectRetina: true
+    }),
+    [mode]
+  )
+
+  if (init) {
+    return (
+      <Particles
+        id='tsparticles'
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    )
+  }
+
+  return <></>
+}
