a225bee87c5af6618d22fbf1b219de10146f155f
Daniel9822 <dhdany64@gmail.com>
2024-02-01 19:53:29 -0400

    update

---
+++ src/types/type.d.ts
@@ -0,0 +1,15 @@
+
+export interface FormStepTwo {
+  userId: string
+  stepTwo: {
+    address: string
+    postalCode: string
+  }
+}
+export interface FormOneStep {
+  userId?: string
+  name: string
+  lastName: string
+  country: string
+  phone: string
+}
