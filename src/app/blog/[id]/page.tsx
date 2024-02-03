95fa874b86e4dba2eeb0886ef8481d2a3a76cc2e
Daniel9822 <dhdany64@gmail.com>
2024-02-03 10:48:24 -0400

    update

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,17 +1,38 @@
-import { mock } from '@/app/dashboard/mock'
+import { getOneBlog } from '@/services/blogs.services'
+import styles from '../blog.module.css'
+import { Metadata, ResolvingMetadata } from 'next'
 
-const getOneBlog = async (id: string) => {
-  return mock.find((e) => e.id === id)
+export async function generateMetadata(
+  { params },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const data = await getOneBlog(params.id)
+
+  return {
+    title: `Blog-auth | ${data.title}`,
+    description: data.desc,
+
+  }
 }
 
-export default async function BlogId({ params } ) {
+export default async function BlogId({ params }) {
   const data = await getOneBlog(params.id)
+
   return (
     <>
-      {data?.id ? (
-        <div>
+      {data?._id ? (
+        <section>
+          <div className={styles.imgContainer}>
+            <img
+              className={styles.blogImage}
+              src={data.image}
+              alt='blog picture'
+            />
+            <span className={styles.author}>{data.author}</span>
+          </div>
           <span>{data.desc}</span>
-        </div>
+        </section>
       ) : null}
     </>
   )
