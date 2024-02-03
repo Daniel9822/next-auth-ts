6a1cc3c0830daf1644461271be2c1f84519d69be
Daniel9822 <dhdany64@gmail.com>
2024-02-03 10:47:59 -0400

    feat: blog services

---
+++ src/services/blogs.services.ts
@@ -0,0 +1,43 @@
+import { Blogs } from '@/types/type'
+
+const PATH = process.env.NEXT_PUBLIC_API_URL
+
+export const createBlog = async (blog: Blogs) => {
+  try {
+    const response = await fetch(`${PATH}/blog`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blog)
+    })
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    throw new Error('somenthing went wrong')
+  }
+}
+
+export const getAllBlog = async (): Promise<Blogs[]> => {
+  try {
+    const response = await fetch(`${PATH}/blog`)
+    const data = response.json()
+    return data
+  } catch (error: any) {
+    console.log(error.message)
+    throw new Error('')
+  }
+}
+
+export const getOneBlog = async (id: string): Promise<Blogs> => {
+  try {
+    const response = await fetch(`${PATH}/blog/${id}`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error);
+    throw new Error('somenthing went wrong')
+  }
+}
